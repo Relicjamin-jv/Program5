c1513ba8486a8343a7272052317194129a2295d1
Relicjamin-jv <campbellcl19@gcc.edu>
2020-04-12 19:40:25 -0400

    Parent of GameState Classes and used by the GameStateMang

---
+++ Program5/src/GS.java
@@ -0,0 +1,16 @@
+import java.awt.Graphics2D;
+
+public abstract class GS {
+	
+	private GameStateMang gs;
+	
+	public GS(GameStateMang gsm) {
+		this.gs = gsm;
+	}
+	
+
+
+	public abstract void update();
+	public abstract void input(KeyHandler key);
+	public abstract void render(Graphics2D g);
+}
